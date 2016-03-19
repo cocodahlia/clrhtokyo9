b3cf90c5167538af560cdeb67fbee76c9e748c9c
Dahlia* <dahlia_cocoa@moonfire.info>
2016-03-19 05:35:07 +0000

    add encoding

--- xss.php
+++ xss.php
@@ -1,3 +1,5 @@
-<html><body>
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head>    
+<body>
 <?php echo "ドーモ、" . $_GET["name"] ."=サン。"?>
 </body></html>
--- xss_fixed.php
+++ xss_fixed.php
@@ -1,3 +1,5 @@
-<html><body>
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head>
+<body>
 <?php echo "ドーモ、" . htmlspecialchars($_GET["name"],ENT_QUOTES,"UTF-8") ."=サン。"?>
 </body></html>
